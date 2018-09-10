95355177772b277225c8e71f9abf2e2d17281e5d
Rita97 <39827149+Rita97@users.noreply.github.com>
2018-09-10 13:59:38 +0200

    Create normalnovekvopiramis09.10.cpp

---
+++ normalnovekvopiramis09.10.cpp
@@ -0,0 +1,13 @@
+  void piramis(int novekvo ){
+
+
+    for(int i=1; i<novekvo+1;i++){
+        
+        for (int j=1; j<i+1; j++){
+        
+            cout<<j;
+        }
+        cout <<   endl;
+    }
+    
+}
